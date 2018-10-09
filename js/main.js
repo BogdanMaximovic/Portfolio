756bc174be55080dc84e0189e5d54e18bdc06c53
Bogdan <42995409+BogdanMaximovic@users.noreply.github.com>
2018-10-09 17:23:04 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('#main-nav a').click(function() {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1200);
+        return false;
+    })
+})
