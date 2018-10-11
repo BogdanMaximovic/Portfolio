87f876e1ac20aae94b5aa2a467eac8e9901db996
Bogdan <42995409+BogdanMaximovic@users.noreply.github.com>
2018-10-11 14:09:59 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,4 +5,16 @@ $(document).ready(function() {
         }, 1200);
         return false;
     })
+  var showText = function (target, message, index, interval) {    
+	 if (index < message.length) { 
+	    $(target).append(message[index++]); 
+	    setTimeout(function () { showText(target, message, index, interval); }, interval); 
+	  } 
+	}
+    
+	$(function () { 
+	 
+	  showText("#msg", "Bogdan Maksimović", 0, 150);    
+	 
+	}); 
 })
