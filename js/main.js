7b5a670f1bd509bb3bf9a789946d9a97165778f5
Bogdan <42995409+BogdanMaximovic@users.noreply.github.com>
2018-11-15 21:51:19 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $('#main-nav a').click(function() {
         $('html, body').animate({
-            scrollTop: $($.attr(this, 'href')).offset().top
+            scrollTop: $($.attr(this, 'href')).offset().top -100
         }, 1200);
         return false;
-    }) 
-})
+    });
+});
