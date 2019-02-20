be3e85801247d6b25259562a3967f5d53c92c1d6
Bogdan <42995409+BogdanMaximovic@users.noreply.github.com>
2019-02-20 18:22:59 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
         return false;
     });
 
-    $('.project-window').attr('data-aos-duration', '900');
+    $('.project-window').attr('data-aos-duration', '500');
     AOS.init();
 
     $('#toggle').click(function() {
@@ -18,5 +18,11 @@ $(document).ready(function() {
         $('#toggle').toggleClass('active');
         $('#overlay').toggleClass('open');
     });
-
 });
+
+function download(){  
+	let link = document.createElement('a')
+    link.download = "Bogdan Maksimović CV";
+    link.href = "Bogdan Maksimović.pdf";
+    link.click();
+}
